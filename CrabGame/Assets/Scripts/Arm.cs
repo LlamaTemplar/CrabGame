e0a500ff35ef2ae345a7ea0a71468579751890c3
Anthony Sokry <anthonysokry11@gmail.com>
2020-06-02 19:37:32 -0700

    combat-player actionsv1.2, player & arm healthv1.0
    Player Actions, Player Health, and Arm Health update and needs review

---
+++ CrabGame/Assets/Scripts/Arm.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    private int startingHP = 20;
+    public float currentHP;
+    public bool loseArm = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = startingHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHP <= 0)
+        {
+            loseArm = true;
+        }
+        else if (currentHP >= startingHP)
+        {
+            currentHP = startingHP;
+            loseArm = false;
+        }
+
+        if (loseArm)
+        {
+            currentHP += Time.deltaTime;
+        }
+    }
+
+    public void TakeDamamge(int dmg)
+    {
+        if (loseArm == false)
+        {
+            currentHP -= dmg;
+        }
+    }
+}
--- CrabGame/Assets/Scripts/PlayerActions.cs
+++ CrabGame/Assets/Scripts/PlayerActions.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerActions : MonoBehaviour
 {
     //Right Arm variables
-    public Collider2D rightArm;
+    public GameObject rightArm;
     public float rightCooldown;
     public float rightStartTime = 2f;
     private float rightDelay;
@@ -13,7 +13,7 @@ public class PlayerActions : MonoBehaviour
     private bool rightWindUp = false;
 
     //Left Arm variables 
-    public Collider2D leftArm;
+    public GameObject leftArm;
     public float leftCooldown;
     public float leftStartTime = 2f;
     private float leftDelay;
@@ -40,19 +40,28 @@ public class PlayerActions : MonoBehaviour
         if (blockCooldown <= 0)
         {
             blockCooldown = 0;
-            //if both arm keys are pressed and the both their delays are greater than 0
-            if ((Input.GetKey(KeyCode.G) && Input.GetKey(KeyCode.F)) && (rightDelay > 0 || leftDelay > 0))
+            //Check if we still have both arms for blocking
+            if (leftArm.GetComponent<Arm>().loseArm == false && rightArm.GetComponent<Arm>().loseArm == false)
             {
-                isBlocking = true;
-                //blocking code
-                Blocking(isBlocking);
+                //if both arm keys are pressed and the both their delays are greater than 0
+                if ((Input.GetKey(KeyCode.G) && Input.GetKey(KeyCode.F)) && (rightDelay > 0 || leftDelay > 0))
+                {
+                    isBlocking = true;
+                    //blocking code
+                    Blocking(isBlocking);
+                }
+                else if ((Input.GetKeyUp(KeyCode.G) || Input.GetKeyUp(KeyCode.F)) && isBlocking == true) //while blocking, when keys are released undo blocking
+                {
+                    //Blocking cooldown begins counting down
+                    blockCooldown = startBlockTime;
+                    isBlocking = false;
+                    //undoing block code (same code)
+                    Blocking(isBlocking);
+                }
             }
-            else if ((Input.GetKeyUp(KeyCode.G) || Input.GetKeyUp(KeyCode.F)) && isBlocking == true) //while blocking, when keys are released undo blocking
+            else //if arm(s) is missing while blocking then undo blocking
             {
-                //Blocking cooldown begins counting down
-                blockCooldown = startBlockTime;
                 isBlocking = false;
-                //undoing block code (same code)
                 Blocking(isBlocking);
             }
         }
@@ -66,18 +75,27 @@ public class PlayerActions : MonoBehaviour
         if (rightCooldown <= 0)
         {
             rightCooldown = 0;
-            //When key is pressed and the delay is at starting number
-            if (Input.GetKeyDown(KeyCode.G) && rightDelay == rightStartDelay) 
+            //check if we still have right arm
+            if (rightArm.GetComponent<Arm>().loseArm == false)
             {
-                //the wind up is true
-                rightWindUp = true;
-            }
+                //When key is pressed and the delay is at starting number
+                if (Input.GetKeyDown(KeyCode.G) && rightDelay == rightStartDelay)
+                {
+                    //the wind up is true
+                    rightWindUp = true;
+                }
 
-            //while wind up is true (i don't like while loops)
-            if (rightWindUp == true)
+                //while wind up is true (i don't like while loops)
+                if (rightWindUp == true)
+                {
+                    //delay will count down
+                    rightDelay -= Time.deltaTime;
+                }
+            }
+            else //if arm is lost or becomes lost during delay, reset delay and windup
             {
-                //delay will count down
-                rightDelay -= Time.deltaTime;
+                rightDelay = rightStartDelay;
+                rightWindUp = false;
             }
 
             //if delay counts down to 0 and Player "is not" blocking, commence attack
@@ -106,18 +124,27 @@ public class PlayerActions : MonoBehaviour
         if (leftCooldown <= 0)
         {
             leftCooldown = 0;
-            //When key is pressed and the delay is at starting number
-            if (Input.GetKeyDown(KeyCode.F) && leftDelay == leftStartDelay)
+            //check if we still have left arm
+            if (leftArm.GetComponent<Arm>().loseArm == false)
             {
-                //the wind up is true
-                leftWindUp = true;
-            }
+                //When key is pressed and the delay is at starting number
+                if (Input.GetKeyDown(KeyCode.F) && leftDelay == leftStartDelay)
+                {
+                    //the wind up is true
+                    leftWindUp = true;
+                }
 
-            //while wind up is true (i don't like while loops)
-            if (leftWindUp == true)
+                //while wind up is true (i don't like while loops)
+                if (leftWindUp == true)
+                {
+                    //delay will count down
+                    leftDelay -= Time.deltaTime;
+                }
+            }
+            else //if arm is lost or becomes lost during delay, reset delay and windup
             {
-                //delay will count down
-                leftDelay -= Time.deltaTime;
+                leftDelay = leftStartDelay;
+                leftWindUp = false;
             }
 
             //if delay counts down to 0 and Player "is not" blocking, commence attack
