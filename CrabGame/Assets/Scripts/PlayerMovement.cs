de5a15e65c5dc059210d0b9132482ebf95eb04d4
TommyZakh <tommyzakh@gmail.com>
2020-06-06 23:12:16 -0700

    Pushing movement code from Unity Teams

---
+++ CrabGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	//Walking
+	public float walkSpeed = 6;
+	public float notStrafingPenalty = 0.67f;
+	public float whileRotatingPenalty = 0.5f;
+
+	//Rotating
+	public float rotateSpeed = 60; //In degrees per second
+
+	// Update is called once per frame
+	void Update()
+	{
+		float deltaTime = Time.deltaTime;
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		bool isRotating = Mathf.Abs(h) > Mathf.Epsilon && Mathf.Abs(v) > Mathf.Epsilon;
+
+		if (isRotating)
+			transform.Rotate(0, -1 * h * rotateSpeed * deltaTime, 0);
+
+		transform.Translate(new Vector3(h, 0, (isRotating ? v * whileRotatingPenalty : v) * notStrafingPenalty) * walkSpeed * deltaTime, Space.Self);
+
+
+	}
+}
