cfa3b4b48fff4614c0ce136945b399a95218dba3
Cole Pergerson <cole.pergerson@gmail.com>
2020-06-30 12:48:51 -0700

    Added stop sound functions

--- CrabGame/Assets/Sound/SoundPlayer.cs
+++ CrabGame/Assets/Sound/SoundPlayer.cs
@@ -60,11 +60,11 @@ public class SoundPlayer : MonoBehaviour
         }
     }
 
-    public void PlaySound(string _name)
+    public void PlaySound(string name)
     {
         for (int i = 0; i < sounds.Length; i++)
         {
-            if (sounds[i].clipName == _name)
+            if (sounds[i].clipName == name)
             {
 
                 sounds[i].Source.PlayOneShot(sounds[i].clip);
@@ -75,7 +75,44 @@ public class SoundPlayer : MonoBehaviour
 
     public void PlaySound(int index)
     {
-        sounds[index].Source.PlayOneShot(sounds[index].clip);
+        if (index >= sounds.Length)
+            Debug.LogError(gameObject.name + ":  PlaySound request is out of bounds of sounds array)");
+        else
+            sounds[index].Source.PlayOneShot(sounds[index].clip);
+    }
+
+    public void StopSound(string name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].Source.isPlaying && sounds[i].clipName == name)
+            {
+                sounds[i].Source.Stop();
+            }
+        }
+    }
+
+    public void StopSound(int index)
+    {
+        if(index >= sounds.Length)
+        {
+            Debug.LogError(gameObject.name + ":  StopSound request is out of bounds of sounds array)");
+        }
+        else if (sounds[index].Source.isPlaying)
+        {
+            sounds[index].Source.Stop();
+        }
+    }
+
+    public void StopAllSounds()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].Source.isPlaying)
+            {
+                sounds[i].Source.Stop();
+            }
+        }
     }
 
     public int GetSoundsLength()
