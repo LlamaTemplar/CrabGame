a76f0f9a9fcaf81d6e623690838f6bb2690fd13d
Anthony Sokry <anthonysokry11@gmail.com>
2020-07-02 15:29:50 -0700

    Updated Gem and Levelmanger code
    Also removed Survey Link from main menu button and made canvas scalable

--- CrabGame/Assets/Scripts/LevelManager.cs
+++ CrabGame/Assets/Scripts/LevelManager.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
+	public TextMeshProUGUI gemScore;
+	[HideInInspector]
+	public int gemNum = 0;
+
+	private void FixedUpdate()
+	{
+		gemScore.text = gemNum.ToString();
+	}
+
 	public void LoadScene(int index)
 	{
 		Time.timeScale = 1;
