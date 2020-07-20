b79aa24eb6be539540235d99616f81fb46c82800
TommyZakh <tommyzakh@gmail.com>
2020-07-20 14:05:40 -0700

    Added smooth camera

---
+++ CrabGame/Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+	private Transform target;
+
+	[SerializeField]
+	private float zDistance = 10;
+
+	[SerializeField]
+	private float lerpSpeed = 4;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+
+		transform.position = target.position + Vector3.back * zDistance;
+    }
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		float t = Time.fixedDeltaTime * lerpSpeed;
+		transform.position = Vector3.Lerp(transform.position, target.position + Vector3.back * zDistance, t);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, t);
+	}
+}
