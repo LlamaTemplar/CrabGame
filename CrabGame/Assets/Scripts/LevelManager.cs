cb54842127da1ec00ecbdb69b5405b8d28e9886b
TommyZakh <tommyzakh@gmail.com>
2020-07-19 21:45:52 -0700

    Made level manager persist between scenes

--- CrabGame/Assets/Scripts/LevelManager.cs
+++ CrabGame/Assets/Scripts/LevelManager.cs
@@ -5,13 +5,26 @@ using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
-	public TextMeshProUGUI gemScore;
+	//public TextMeshProUGUI gemScore;
 	[HideInInspector]
 	public int gemNum = 0;
 
+	private static LevelManager Instance;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+			Destroy(this);
+	}
+
 	private void FixedUpdate()
 	{
-		gemScore.text = gemNum.ToString();
+		//gemScore.text = gemNum.ToString();
 		
 	}
 
---
+++ CrabGame/Assets/Scripts/TransitionFade.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TransitionFade : MonoBehaviour
+{
+	public Image image;
+
+	public bool transitionState;
+
+    // For OnClick Event for Start Button
+    public void StartGame()
+    {
+        // Load the next Scene in the Build settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // For OnClick Event for Controls Button
+    public void ShowControls()
+    {
+        // Hide Start Menu
+        startMenu.SetActive(false);
+        // Show Controls
+        controls.SetActive(true);
+    }
+
+    // For OnClick Event for Back Button (After Controls are shown)
+    public void HideControls()
+    {
+        // Hide Controls
+        controls.SetActive(false);
+        // Show Start Menu
+        startMenu.SetActive(true);
+    }
+
+    // For OnClick Event for Exit Button
+    public void ExitGame()
+    {
+        // Closes application (Note: will only work if the Application is built and exported, so it will NOT work in the Unity Project)
+        Application.Quit();
+    }
+}
