a2f1c3e2831ed0bd8aa00555b2d70e14f0e1da03
Cole Pergerson <cole.pergerson@gmail.com>
2020-08-08 16:47:53 -0700

    Revert "Merge branch 'levels' into code"
    This reverts commit 0b6d7cd678ad0abae873675355f08844b7802045, reversing
    changes made to 498678df09b6bce27399f5fadd2b7a05cbbf27ea.

--- CrabGame/Assets/Scripts/ScoreManager.cs
+++ CrabGame/Assets/Scripts/ScoreManager.cs
@@ -6,49 +6,35 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-	public TextMeshProUGUI algaeScore;
+	public TextMeshProUGUI gemScore;
 
-	private int algaeNum = 0;
-	//[SerializeField]
-	//public int gemNumCondition = 0;
-	public GameObject AlgaeRootObject;
+	private int gemNum = 0;
+	[SerializeField]
+	public int gemNumCondition = 5;
 
 	private LevelManager levelManager;
 
 	private void Awake()
 	{
-		if (algaeScore == null) algaeScore = GameObject.FindGameObjectWithTag("ScoreText").GetComponent<TextMeshProUGUI>();
-
 		levelManager = GameObject.FindGameObjectWithTag("Level Manager").GetComponent<LevelManager>();
 	}
 
 	private void Start()
 	{
-
-		if(AlgaeRootObject != null)
-		{
-			CountAlgae();
-		}
-
 		UpdateScore();
 	}
 
-	public void RemoveAlgae(int amount)
+	public void AddGems(int amount)
 	{
-		algaeNum -= amount;
+		gemNum += amount;
 		UpdateScore();
 
-		if (algaeNum <= 0)
+		if (gemNum >= gemNumCondition)
 			levelManager.LoadScene(2);
 	}
 
 	private void UpdateScore()
 	{
-		algaeScore.text = algaeNum + "";
-	}
-
-	private void CountAlgae()
-	{
-		algaeNum = AlgaeRootObject.transform.childCount;
+		gemScore.text = gemNum + "";
 	}
 }
