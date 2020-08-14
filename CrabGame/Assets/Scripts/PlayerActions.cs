6c84f18e071d003a30251fd656babbe6aadf849e
Anthony Sokry <anthonysokry11@gmail.com>
2020-08-14 15:09:37 -0700

    Removed Unnecessary code in PlayerActions

--- CrabGame/Assets/Scripts/PlayerActions.cs
+++ CrabGame/Assets/Scripts/PlayerActions.cs
@@ -16,53 +16,32 @@ public class PlayerActions : MonoBehaviour
     private int damage = 40;
 
     // For Both Arms
-    private float incrementByNum = 0.2f;
-    private float currentIncrement = 0f;
-    private float incrementTotal = 2f;
     private Animator animator;
 
     // Right Arm variables
     public GameObject rightArm;
     public float rightCooldown;
     public float rightStartTime = 1f;
-    private float rightDelay;
-    private float rightStartDelay = 0.2f;
-    private bool rightWindUp = false;
-    private Vector3 rightOGpos;
 
     // Left Arm variables 
     public GameObject leftArm;
     public float leftCooldown;
     public float leftStartTime = 1f;
-    private float leftDelay;
-    private float leftStartDelay = 0.2f;
-    private bool leftWindUp = false;
-    private Vector3 leftOGpos;
 
     // Block variables
     public float blockCooldown;
     public float startBlockTime = 1.5f;
     public bool isBlocking = false;
     // Will most likely be removed these 3 lines after adding animations
-    private Vector3 rightBlockPos;
-    private Vector3 leftBlockPos; 
-    public GameObject subBlockSprite;
 
     struct PlayerArm
     {
-        public GameObject arm;     
-        //public bool windUp;
-        
-        
-        public Vector3 startingPos;
         public ArmSide side;
     }
 
     PlayerArm[] arms = new PlayerArm[2];
     enum ArmSide {Left, Right};
     private PlayerArm attackingArm;
-
-
 
 
     // Start is called before the first frame update
@@ -74,22 +53,8 @@ public class PlayerActions : MonoBehaviour
 
 
         arms[0].side = ArmSide.Left;
-        arms[0].startingPos = leftArm.transform.localPosition;
 
         arms[1].side = ArmSide.Right;
-        arms[1].startingPos = rightArm.transform.localPosition;
-
-        // Delays for the attacks
-        //rightDelay = rightStartDelay;
-        //leftDelay = leftStartDelay;
-
-        // Store original Local Positions (Might also be removed after adding animation)
-        rightOGpos = rightArm.transform.localPosition;
-        leftOGpos = leftArm.transform.localPosition;
-
-        // Store Block positions
-        rightBlockPos = new Vector3(rightArm.transform.localPosition.x - 0.05f, rightArm.transform.localPosition.y, rightArm.transform.localPosition.z);
-        leftBlockPos = new Vector3(leftArm.transform.localPosition.x + 0.05f, leftArm.transform.localPosition.y, leftArm.transform.localPosition.z);
 
         animator = GetComponentInChildren<Animator>();
         if (animator == null)
@@ -141,7 +106,6 @@ public class PlayerActions : MonoBehaviour
         {
             postAttackCoolDown -= Time.deltaTime;
         }
-
     }
 
     bool HasLeftPunchInput()
@@ -228,11 +192,6 @@ public class PlayerActions : MonoBehaviour
         // Make player bool false, to prevent taking damage
         gameObject.GetComponent<Player>().isBlocking = b;
         // Stop Blocking Animation
-        rightArm.transform.localPosition = rightOGpos;
-        leftArm.transform.localPosition = leftOGpos;
-        rightArm.GetComponent<Arm>().SetCollider(b);
-        leftArm.GetComponent<Arm>().SetCollider(b);
-        subBlockSprite.SetActive(false);
         SetAnimations("block", false);
         SetAnimatorSpeed(1f);
     }
@@ -267,11 +226,6 @@ public class PlayerActions : MonoBehaviour
         // Make player bool true, to prevent taking damage
         gameObject.GetComponent<Player>().isBlocking = b;
         // Blocking Animation
-        rightArm.transform.localPosition = rightBlockPos;
-        leftArm.transform.localPosition = leftBlockPos;
-        rightArm.GetComponent<Arm>().SetCollider(b);
-        leftArm.GetComponent<Arm>().SetCollider(b);
-        subBlockSprite.SetActive(true);
         SetAnimations("block",true);
     }
 
@@ -285,100 +239,6 @@ public class PlayerActions : MonoBehaviour
         {
             gameObject.GetComponent<Player>().PlayMissSound();
         }
-    }
-
-    void RightAttack()
-    {
-        // Double check if the Player is not blocking
-        if (isBlocking == false)
-        {
-            // Deal damge if enemy is in hitbox
-            rightArm.GetComponent<Arm>().SetAttackingTrue();
-            rightArm.GetComponent<Arm>().SetCollider(true);
-
-            // Right Arm Attack animation
-
-            // Move arm hitbox into position to deal damage
-            if (currentIncrement < incrementTotal)
-            {
-                if (currentIncrement == 0)
-                {
-                    gameObject.GetComponent<Unit>().PlayPunchingSound();
-                    SetAnimations("right", true);
-                }
-                currentIncrement += incrementByNum;
-                rightArm.transform.localPosition = new Vector3(rightArm.transform.localPosition.x, rightArm.transform.localPosition.y + incrementByNum, rightArm.transform.localPosition.z);
-            }
-            else
-            {
-                ResetRightArm();
-            }
-        }
-    }
-
-    void ResetRightArm()
-    {
-        rightArm.GetComponent<Arm>().SetCollider(false);
-        rightArm.GetComponent<Arm>().SetAttackingFalse();
-
-        if (rightArm.GetComponent<Arm>().GetTheEnemy() == null)
-        {
-            gameObject.GetComponent<Unit>().PlayMissSound();
-        }
-        rightArm.GetComponent<Arm>().SetTheEnemyNull();
-
-        currentIncrement = 0f;
-        // If attacking then reset delay first so that attack only happens once and doesn't loop
-        rightDelay = rightStartDelay;
-        // Begin cooldown
-        rightCooldown = rightStartTime;
-    }
-
-    void LeftAttack()
-    {
-        // Double check if the Player is not blocking
-        if (isBlocking == false)
-        {
-            // Deal damge if enemy is in hitbox
-            leftArm.GetComponent<Arm>().SetAttackingTrue();
-            leftArm.GetComponent<Arm>().SetCollider(true);
-
-            // Left Arm Attack animation
-
-            // Move arm hitbox into position to deal damage
-            if (currentIncrement < incrementTotal)
-            {
-                if (currentIncrement == 0)
-                {
-                    gameObject.GetComponent<Unit>().PlayPunchingSound();
-                    SetAnimations("left",true);
-                }
-                currentIncrement += incrementByNum;
-                leftArm.transform.localPosition = new Vector3(leftArm.transform.localPosition.x, leftArm.transform.localPosition.y + incrementByNum, leftArm.transform.localPosition.z);
-            }
-            else
-            {
-                ResetLeftArm();
-            }
-        }
-    }
-
-    void ResetLeftArm()
-    {
-        leftArm.GetComponent<Arm>().SetCollider(false);
-        leftArm.GetComponent<Arm>().SetAttackingFalse();
-
-        if (leftArm.GetComponent<Arm>().GetTheEnemy() == null)
-        {
-            gameObject.GetComponent<Unit>().PlayMissSound();
-        }
-        leftArm.GetComponent<Arm>().SetTheEnemyNull();
-
-        currentIncrement = 0f;
-        // If attacking then reset delay first so that attack only happens once and doesn't loop
-        leftDelay = leftStartDelay;
-        // Begin cooldown
-        leftCooldown = leftStartTime;
     }
 
     public void SetAnimations(string anim, bool b)
